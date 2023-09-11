6101db05d109f96f40a7e811e58938d4196add2d
bBilRa <suslik228o@gmail.com>
2023-09-11 14:19:06 +0500

    Закрытие массивов, конец основного кода, необходимо добавить обратный перевод

--- Morze/main.cpp
+++ Morze/main.cpp
@@ -14,12 +14,71 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	string s;
+	int x;
+	string tran = "Морзе";
+	bool bu = true;
+	do
+	{
 
-	cout << "Введите текст: ";
-	cin >> s;
-	cout << s;
-	Transform tr;
-	s = tr.trans(s);
-	cout << endl << s;
+		while (tran == "Морзе")
+		{
+			cout << "Введите текст перевода с русского или meny: ";
+			getline(cin, s);
+			if (s != "meny") {
+				Transform tr;
+				s = tr.trans(s);
+
+				cout << endl << s << endl;
+			}
+			else if (s == "meny")
+			{
+				cout << "Добро пожаловать в МЕНЮ, вот что вы можете сделать:" << endl;
+				cout << "1)Завершить программу" << endl << "2)Перевести с морзы" << endl << "3)Продолжить перевод с русскокго" << endl << "Ваш вариант: ";
+				cin >> x;
+				switch (x)
+				{
+				case 1:
+					tran = "0";
+					bu == false;
+					break;
+				case 2:
+					tran = "Рус";
+					break;
+				case 3:
+					tran = "Морзе";
+				}
+			}
+		}
+		while (tran == "Рус")
+		{
+			cout << "Введите текст перевода или meny: ";
+			getline(cin, s);
+			if (s != "meny") {
+				Transform tr;
+				s = tr.trans(s);
+
+				cout << endl << s << endl;
+			}
+			else if (s == "meny")
+			{
+				cout << "Добро пожаловать в МЕНЮ, вот что вы можете сделать:" << endl;
+				cout << "1)Завершить программу" << endl << "2)Продолжить перевод с морзы" << endl << "3)Перевод с русскокго" << endl << "Ваш вариант: ";
+				cin >> x;
+				switch (x)
+				{
+				case 1:
+					tran = "0";
+					bu == false;
+					break;
+				case 2:
+					tran = "Рус";
+					break;
+				case 3:
+					tran = "Морзе";
+				}
+			}
+		}
+	} while (bu == false);
 	return 0;
 }
+	
